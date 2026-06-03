0ea26d417ac74ed9051ab1bac4fcf13a9064c40f
doter2410 <doter24.10@gmail.com>
2026-06-03 10:42:44 +0500

    finished feature/users done

--- internal/features/users/transport/http/patch_user.go
+++ internal/features/users/transport/http/patch_user.go
@@ -1,7 +1,9 @@
 package users_transport_http
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/doter2410/NewToDo/internal/core/domain"
 	core_logger "github.com/doter2410/NewToDo/internal/core/logger"
@@ -14,6 +16,36 @@ import (
 type PatchUserRequest struct {
 	FullName    core_http_types.Nullabel[string] `json:"full_name"`
 	PhoneNumber core_http_types.Nullabel[string] `json:"phone_number"`
+}
+
+func (r *PatchUserRequest) Validate() error {
+	if r.FullName.Set {
+		if r.FullName.Value == nil {
+			return fmt.Errorf("FullName can't be NULL")
+		}
+
+		fullNameLen := len([]rune(*r.FullName.Value))
+		if fullNameLen < 3 || fullNameLen > 100 {
+			return fmt.Errorf("FullName must be between 3 and 100 symbols")
+		}
+	}
+
+	if r.PhoneNumber.Set {
+		if r.PhoneNumber.Value != nil {
+			phoneNumberLen := len([]rune(*r.PhoneNumber.Value))
+			if phoneNumberLen < 10 || phoneNumberLen > 15 {
+				return fmt.Errorf("PhoneNumber must be between 10 and 15 symbols")
+			}
+
+			if !strings.HasPrefix(*r.PhoneNumber.Value, "+") {
+				return fmt.Errorf("PhoneNumber must startswith + symbol")
+			}
+
+		}
+
+	}
+
+	return nil
 }
 
 type PatchUserResponse UserDTOResponse
